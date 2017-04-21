a40f93a61642ce8d0ff6168f8ecef57dc6fc2877
Gajanan Pojage <pojage@gmail.com>
2017-04-21 13:04:52 +0530

    e2e updates

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,46 +2,40 @@ import { CarrouselPage } from './app.po';
 
 describe('carrousel App', () => {
   let page: CarrouselPage;
-   var beforeImage = "/assets/images/dest1.png";
-    var afterImage = "/assets/images/dest2.png";
+  var beforeImage = "/assets/images/dest1.png";
+  var afterImage = "/assets/images/dest2.png";
   beforeEach(() => {
     page = new CarrouselPage();
   });
 
   it('should have 4 slides for carousel ', () => {
     page.navigateTo();
- expect(page.getSlidesCount().count()).toEqual(4);
+    expect(page.getSlidesCount().count()).toEqual(4);
   });
 
 
- it('should have 2 controll buttons for carousel ', () => {
+  it('should have 2 controll buttons for carousel ', () => {
     page.navigateTo();
- expect(page.getControlButtonsCount().count()).toEqual(2);
+    expect(page.getControlButtonsCount().count()).toEqual(2);
   });
 
-    it('should have 1 indicator for carousel ', () => {
+  it('should have 1 indicator for carousel ', () => {
     page.navigateTo();
- expect(page.getIndicatorsCount().count()).toEqual(1);
+    expect(page.getIndicatorsCount().count()).toEqual(1);
   });
 
 
-
-    it('should have 1 indicator for carousel ', () => {
-    page.navigateTo();
- expect(page.getIndicatorsCount().count()).toEqual(1);
-  });
-
-      it('should have first image followed by second one on clicking right control  ', () => {
+  it('should have first image followed by second one on clicking right control  ', () => {
     page.navigateTo();
     expect(page.getActiveImage().getAttribute('src')).toBe(beforeImage);
     page.getNextButton().click();
-    expect(page.getActiveImage().getAttribute('src')).toBe(afterImage );
+    expect(page.getActiveImage().getAttribute('src')).toBe(afterImage);
   });
 
-       it('should have second image followed by first one on clicking left control  ', () => {
+  it('should have second image followed by first one on clicking left control  ', () => {
     page.navigateTo();
-   page.getNextButton().click();
-    expect(page.getActiveImage().getAttribute('src')).toBe(afterImage );
+    page.getNextButton().click();
+    expect(page.getActiveImage().getAttribute('src')).toBe(afterImage);
     page.getPreviousButton().click();
     expect(page.getActiveImage().getAttribute('src')).toBe(beforeImage);
   });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,24 +5,24 @@ export class CarrouselPage {
     return browser.get('/');
   }
 
-   getSlidesCount(){
-   return  element.all(by.css(".carousel-slider-img"));
+  getSlidesCount() {
+    return element.all(by.css(".carousel-slider-img"));
   }
 
-  getControlButtonsCount(){
-   return  element.all(by.css(".carousel-control"));
+  getControlButtonsCount() {
+    return element.all(by.css(".carousel-control"));
   }
 
-    getIndicatorsCount(){
-   return  element.all(by.css(".carousel-indicators"));
+  getIndicatorsCount() {
+    return element.all(by.css(".carousel-indicators"));
   }
-   getPreviousButton(){
-     return element(by.css('.left .carousel-control')); 
-   }
-      getNextButton(){
-     return element(by.css('.right .carousel-control')); 
-   }
-       getActiveImage (){
-     return element(by.css('slide.active')); 
-   }
+  getPreviousButton() {
+    return element(by.css('.left .carousel-control'));
+  }
+  getNextButton() {
+    return element(by.css('.right .carousel-control'));
+  }
+  getActiveImage() {
+    return element(by.css('slide.active'));
+  }
 }
